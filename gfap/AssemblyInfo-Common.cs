1284b3bcf81da8dccfcbd71d25a2bc56a2769dd2
wigy <wigy_opensource_developer@yahoo.com>
2015-05-18 21:50:30 +0200

    Added solution + portable domain assembly

---
+++ gfap/AssemblyInfo-Common.cs
@@ -0,0 +1,17 @@
+﻿using System.Resources;
+using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("G2 Speech B.V.")]
+[assembly: AssemblyProduct("gfap")]
+[assembly: AssemblyCopyright("Copyright © G2 Speech B.V. 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ gfap/Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Core")]
+[assembly: AssemblyDescription("The domain assembly having no dependencies")]
+
