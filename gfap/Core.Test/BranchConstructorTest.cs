5a3a70035c0afe8976ed23ccdaed9682a403d452
wigy <wigy_opensource_developer@yahoo.com>
2015-09-05 17:51:50 +0200

    It is so easy to get 100% code coverage so early in a project and deep in the core.

---
+++ gfap/Core.Test/BranchConstructorTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace Wigy.Gfap.Core.Test
+{
+    public class BranchConstructorTest
+    {
+        [Fact]
+        void StartsWithNoRevisions()
+        {
+            var branch = new Branch(null);
+
+            Assert.Empty(branch.Revisions);
+        }
+
+        [Fact]
+        void SetsBasedOnProperty()
+        {
+            var baseBranch = new Branch(null);
+            var branch = new Branch(baseBranch);
+
+            Assert.Same(baseBranch, branch.BasedOn);
+        }
+    }
+}
